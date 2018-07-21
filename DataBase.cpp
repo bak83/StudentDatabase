53ff1dd188242e894e1cba41ec6bfa0d6c7f0548
just44play <justyna.walkowiak6969@gmail.com>
2018-07-21 20:21:52 +0200

    ajust to employee class

--- DataBase.cpp
+++ DataBase.cpp
@@ -1,18 +1,29 @@
 #include "DataBase.hpp"
 
-void DataBase::addNewStudent(Student s)
+void DataBase::addNewRecord(Record* r)
+{
+    dataBase.push_back(r);
+}
+
+void DataBase::displayRecordList()
+{
+    std::cout << "BAZA DANYCH:" << std::endl;
+    for (int i = 0; i < dataBase.size(); ++i) {
+        std::cout << i+1 << ". " << dataBase.at(i)->getName() << "  "
+                  << dataBase.at(i)->getSurname() << std::endl;
+    }
+}
+
+/*void DataBase::addNewStudent(Student s)
 {
     studentBase.push_back(s);
 }
 
 void DataBase::sortStudentsByIndex()
 {  
-    for (int i = 1; i < studentBase.size(); ++i)
-    {
-        for (int j = studentBase.size()-1; j >= 1; j--)
-        {
-            if (studentBase.at(j).getIndex() < studentBase.at(j-1).getIndex())
-            {
+    for (int i = 1; i < studentBase.size(); ++i) {
+        for (int j = studentBase.size()-1; j >= 1; j--) {
+            if (studentBase.at(j).getIndex() < studentBase.at(j-1).getIndex()) {
                 Student buff;
                 buff = studentBase.at(j-1);
                 studentBase.at(j-1) = studentBase.at(j);
@@ -24,10 +35,8 @@ void DataBase::sortStudentsByIndex()
 
 void DataBase::removeStudentByIndex(uint64_t idx)
 {
-    for (int i = 0; i < studentBase.size(); ++i)
-    {
-        if (idx == studentBase.at(i).getIndex())
-        {
+    for (int i = 0; i < studentBase.size(); ++i) {
+        if (idx == studentBase.at(i).getIndex()) {
             studentBase.erase(studentBase.begin() + i);
             break;
         }
@@ -37,12 +46,11 @@ void DataBase::removeStudentByIndex(uint64_t idx)
 void DataBase::displayStudentList()
 {
     std::cout << "LISTA STUDENTOW:" << std::endl;
-    for (int i = 0; i < studentBase.size(); ++i)
-    {
+    for (int i = 0; i < studentBase.size(); ++i) {
         std::cout << i+1 << ". " << studentBase.at(i).getName() << "  "
                   << studentBase.at(i).getSurname() << "  "
                   << studentBase.at(i).getIndex() << std::endl;
     }
 
     std::cout << std::endl;
-}
+}*/
--- DataBase.hpp
+++ DataBase.hpp
@@ -1,16 +1,22 @@
 #pragma once
 
+#include "Record.hpp"
 #include "Student.hpp"
+#include "Employee.hpp"
+
 #include <vector>
 
 class DataBase
 {
-    std::vector<Student> studentBase;
+   // std::vector<Student> studentBase;
+    std::vector<Record*> dataBase;
 
  public:
-    void addNewStudent(Student);
+    void addNewRecord(Record*);
+    void displayRecordList();
+   /* void addNewStudent(Student);
     void sortStudentsByIndex();
     void removeStudentByIndex(uint64_t);
-    void displayStudentList();
+    void displayStudentList();*/
 
 };
